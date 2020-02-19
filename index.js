379bd245a6045df2e0cccfe705418f0922d68da9
Brenton <bjackson71@gmail.com>
2020-02-18 23:29:38 -0500

    added js to footer and linked to github repo

---
+++ index.js
@@ -0,0 +1,19 @@
+var footer = document.getElementById("footer");
+var primary = "hsl(218, 38%, 50%)";
+var secondary = "hsl(0, 0%, 14%)";
+
+footer.addEventListener('mouseover',changeColor);
+footer.addEventListener('mouseout', changeBack);
+footer.addEventListener('click', toProfile);
+
+function changeColor() {
+    this.style.background = primary;
+}
+
+function changeBack() {
+    this.style.background = secondary;
+}
+
+function toProfile() {
+    window.location='http://github.com/dabrentmeister/survey';
+}
